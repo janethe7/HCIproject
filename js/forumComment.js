ef117e019dc62bc26b5750ee4c76232df437da70
Diana Thai <dianathai97@gmail.com>
2018-11-15 19:45:48 -0800

    forum changes

---
+++ js/forumComment.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+    }
+
+function myFunction2() {
+    var y = document.getElementById("myText2").value;
+    document.getElementById("demo2").innerHTML = y;
+    }
+
+function myFunction3() {
+    var z = document.getElementById("myText3").value;
+    document.getElementById("demo3").innerHTML = z;
+    }
+
+
+
+
+
+
