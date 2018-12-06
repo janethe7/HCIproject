9468cca736586d9bb6cb69f3468299a915800877
Diana Thai <dianathai97@gmail.com>
2018-12-05 23:38:45 -0800

    css for maps page

--- js/profile.js
+++ js/profile.js
@@ -34,6 +34,34 @@ function processSignup(){
     localStorage.setItem("email", email);
     localStorage.setItem("password2", password2);
 
+    function validateEmail(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+    
+
+      var $result = $("#result");
+      var email = $("#email").val();
+      $result.text("");
+    
+      if (validateEmail(email)) {
+        $result.text(email + " is valid :)");
+        $result.css("color", "green");
+      } else {
+        $result.text(email + " is not valid :(");
+        $result.css("color", "red");
+      }
+
+
+      if (password1 !== confirmPassword) {
+      alert('Passwords do not match. Please try again!');
+  }
+  else {
+      window.open("profile.html");
+  } 
+
+    }
+
   /*  const sqlite3 = require('sqlite3').verbose();
 
     let db = new sqlite3.Database('../js/userDB.db');
@@ -47,12 +75,8 @@ function processSignup(){
      });
 */
 
-  if (password1 !== confirmPassword) {
-      alert('Passwords do not match. Please try again!');
-  }
-  else {
-      window.open("profile.html");
-  }
+
+
 }
 
 function addTool(){
